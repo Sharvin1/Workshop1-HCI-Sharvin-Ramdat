6878324aacd13f1b1ef0fab767e696b7ff92dccb
Sharvin1 <sharvin@hotmail.nl>
2020-05-16 22:19:06 +0200

    rotationSpeed*(2-rotationSpeed)

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,9 +5,9 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
-	myOtherBox.object3D.rotation.y -= rotationSpeed;
-	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	myOtherBox.object3D.rotation.x -= rotationSpeed*(2-rotationSpeed); //t => t*(2-t)
+	myOtherBox.object3D.rotation.y -= rotationSpeed*(2-rotationSpeed);
+	myOtherBox.object3D.rotation.z -= rotationSpeed*(2-rotationSpeed);
 
 	myOtherBox.object3D.rotation = easeOutQuad;
 	console.log(myOtherBox.object3D.rotation.x);
