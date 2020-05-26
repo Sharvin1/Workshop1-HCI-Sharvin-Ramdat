6d1830cde4bd93bb9e1bfc337f0a779411092fb3
Sharvin1 <sharvin@hotmail.nl>
2020-05-26 17:24:05 +0200

    rotation fixed

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,12 +5,11 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.x -= rotationSpeed*2;
 
-	myOtherBox.object3D.rotation.y -= rotationSpeed*(2-rotationSpeed);
-    myOtherBox.object3D.rotation.z -= rotationSpeed*(2-rotationSpeed);
+	myOtherBox.object3D.rotation.y += rotationSpeed*2;
+    myOtherBox.object3D.rotation.z += rotationSpeed*2;
 
-	//myOtherBox.object3D.rotation = easeOutQuad;
 	console.log(myOtherBox.object3D.rotation.x);
 	//return 1 - pow(1 - rotationSpeed, 3);
 }
